9be93b3a0bceca0ad18089fd05021d35aade14d7
Matt Wilkinson <mattwilki17@gmail.com>
2021-12-13 00:29:14 -0500

    learning

--- lib/learn.ex
+++ lib/learn.ex
@@ -16,3 +16,38 @@ defmodule Learn do
     :world
   end
 end
+
+defmodule Math do
+  def sum(a, b) do
+    do_sum(a, b)
+  end
+
+  defp do_sum(a, b) do
+    a + b
+  end
+
+  def zero?(0) do
+    true
+  end
+
+  def zero?(x) when is_integer(x) do
+    false
+  end
+end
+
+defmodule KV do
+  def start_link do
+    Task.start_link(fn -> loop(%{}) end)
+  end
+
+  defp loop(map) do
+    receive do
+      {:get, key, caller} ->
+        send(caller, Map.get(map, key))
+        loop(map)
+
+      {:put, key, value} ->
+        loop(Map.put(map, key, value))
+    end
+  end
+end
